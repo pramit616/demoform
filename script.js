d5d354c80141d9216614d7e0ea7fe9a81a596600
Pramit Barua <33195858+pramit616@users.noreply.github.com>
2022-08-07 21:14:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,69 @@ const form = document.querySelector("form"),
         backBtn = form.querySelector(".backBtn"),
         allInput = form.querySelectorAll(".first input");
         const issueDateText=form.querySelector("#issueDate");
-        const expDateText=form.querySelector("#expDate");      
+        const expDateText=form.querySelector("#expDate"); 
+        const fullNameText=form.querySelector("#fullName"); 
+        const dobText =form.querySelector("#dob");  
+        const emailText =form.querySelector("#email")
+        const mobileText =form.querySelector("#mobile")
+        const occupationText=form.querySelector("#occupation");
+        const idTypeText=form.querySelector("#idType")
+        const idNumText=form.querySelector("#idNum")
+        const issueAuthText=form.querySelector("#issueAuth")
+        const issueStateText=form.querySelector("#issueState")
+        
        
 
 nextBtn.addEventListener("click", ()=> {
+    const fullName=fullNameText.value;
+    if(fullName.length<1){
+        alert("Name  is Required");
+        return;
+    }
+    const dobValue=dobText.value;
+    if(dobValue===""){
+        alert("Date of Birth is required");
+        return;
+    }
+    const email=emailText.value;
+    if(email===""){
+        alert("Email is Required");
+        return;
+    }
+    const mobile=mobileText.value;
+    if(mobile===""){
+        alert("Mobile Number is Required");
+        return;
+    }
+    const occupation=occupationText.value;
+    if(occupation===""){
+        alert("Occupation is Required");
+        return;
+    }
+    const idType=idTypeText.value;
+    if(idType===""){
+        alert("Id Type is Required");
+        return;
+    }
+    const idNum=idNumText.value;
+    if(idNum===""){
+        alert("Id Number is Required");
+        return;
+    }
+    const issueAuth=issueAuthText.value;
+    if(issueAuth===""){
+        alert("Issue Auth is Required");
+        return;
+    }
+    const issueState=issueStateText.value;
+    if(issueState===""){
+        alert("Issue State is Required");
+        return;
+    }
+   
+    
+   
+
     const issueDate=new Date(issueDateText.value);
     const expDate =new Date(expDateText.value);
     const issue=issueDate.getTime();
